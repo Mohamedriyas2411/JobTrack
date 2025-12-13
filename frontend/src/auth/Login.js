1404f89cd698dfc87328da1956d5d37d6da5d7d5
Mohamedriyas2411 <mohammedriyas2411@gmail.com>
2025-12-13 15:20:21 +0530

    changes to api.js and the Login.js

---
+++ frontend/src/auth/Login.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import api from "../api/api";
+
+function Login({ setRole }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const res = await api.post("/auth/login", { email, password });
+    localStorage.setItem("token", res.data.token);
+    setRole(res.data.role);
+  };
+
+  return (
+    <div>
+      <h2>ServiceFlow Login</h2>
+      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
